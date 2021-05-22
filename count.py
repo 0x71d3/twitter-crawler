e313390271aed9c5a6fe296f2adfc42aadedf40d
0x71d3 <0x71d3@gmail.com>
2021-05-22 12:30:21 +0900

    Update count.py for sub/

--- count.py
+++ count.py
@@ -1,35 +1,33 @@
 import csv
+import glob
 from collections import Counter
 
-# import numpy as np
-# from sklearn import linear_model
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-c_len = Counter()
+print('cleaned.tsv')
+
+c = Counter()
 
 with open('cleaned.tsv', encoding='utf-8') as f:
     reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
     for row in reader:
-        c_len[len(row)] += 1
+        c[len(row)] += 1
 
-print(c_len)
-
-fig, ax = plt.subplots()
-
-ax.bar(c_len.keys(), c_len.values())
-
-plt.show()
-
-# reg = linear_model.LinearRegression()
-
-# X = np.expand_dims(list(c_len.keys()), axis=1)
-# y = np.log(list(c_len.values()))
-
-# reg.fit(X, y)
+print(c)
 
 # fig, ax = plt.subplots()
-
-# ax.plot(X, y, 'o')
-# ax.plot(X, reg.predict(X), 'o')
-
+# ax.bar(c_len.keys(), c_len.values())
 # plt.show()
+
+print('\nsub/')
+
+c_sub = Counter()
+
+for tsv in glob.glob('sub/*.tsv'):
+    with open(tsv, encoding='utf-8', newline='') as f:
+        reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            c_sub[len(row)] += 1
+
+print(c_sub)
